c7e27f30ef85258a91b93698d9538a59cd2e02a9
lucas <lucasa@gmail.com>
2016-09-25 13:41:00 -0300

    Loads politics table at home page.

--- politicos/src/main/webapp/app/home/home.controller.js
+++ politicos/src/main/webapp/app/home/home.controller.js
@@ -5,9 +5,9 @@
         .module('politicosApp')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state'];
+    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state', 'Politic', 'ParseLinks'];
 
-    function HomeController ($scope, Principal, LoginService, $state) {
+    function HomeController ($scope, Principal, LoginService, $state, Politic, ParseLinks) {
         var vm = this;
 
         vm.account = null;
@@ -29,5 +29,58 @@
         function register () {
             $state.go('register');
         }
+
+
+        // load politics collection
+        vm.politics = [];
+        vm.loadPage = loadPage;
+        vm.page = 0;
+        vm.links = {
+            last: 0
+        };
+        vm.predicate = 'id';
+        vm.reset = reset;
+        vm.reverse = true;
+
+        loadAll();
+
+        function loadAll () {
+            Politic.query({
+                page: vm.page,
+                size: 20,
+                sort: sort()
+            }, onSuccess, onError);
+            function sort() {
+                var result = [vm.predicate + ',' + (vm.reverse ? 'asc' : 'desc')];
+                if (vm.predicate !== 'id') {
+                    result.push('id');
+                }
+                return result;
+            }
+
+            function onSuccess(data, headers) {
+                vm.links = ParseLinks.parse(headers('link'));
+                vm.totalItems = headers('X-Total-Count');
+                for (var i = 0; i < data.length; i++) {
+                    vm.politics.push(data[i]);
+                }
+            }
+
+            function onError(error) {
+                AlertService.error(error.data.message);
+            }
+        }
+
+        function reset () {
+            vm.page = 0;
+            vm.politics = [];
+            loadAll();
+        }
+
+        function loadPage(page) {
+            vm.page = page;
+            loadAll();
+        }
+
     }
 })();
