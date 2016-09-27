e39ddb2a5d414873d0c2aebb83dea5d5ceabd3ef
lucas <lucasa@gmail.com>
2016-09-27 01:50:48 -0300

    Load at home page a politics gallery images using angulargrid.js.

--- politicos/src/main/webapp/app/app.module.js
+++ politicos/src/main/webapp/app/app.module.js
@@ -13,7 +13,7 @@
             'ui.bootstrap.datetimepicker',
             'ui.router',
             'infinite-scroll',
-	    'akoenig.deckgrid',
+	    'angularGrid',
             // jhipster-needle-angularjs-add-module JHipster will add new module here
             'angular-loading-bar'
         ])
--- politicos/src/main/webapp/app/home/home.controller.js
+++ politicos/src/main/webapp/app/home/home.controller.js
@@ -1,13 +1,16 @@
 (function() {
     'use strict';
 
-    angular
-        .module('politicosApp')
-        .controller('HomeController', HomeController);
+   angular.module('politicosApp')
+    .service('imageService',['$q','$http',function($q,$http){
+        this.loadImages = function(){
+            return $http.jsonp("https://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=JSON_CALLBACK");
+        };
+    }]).controller('HomeController', HomeController);
 
-    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state', 'Politic', 'ParseLinks'];
+    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state', 'Politic', 'ParseLinks', 'imageService', 'angularGridInstance'];
 
-    function HomeController ($scope, Principal, LoginService, $state, Politic, ParseLinks) {
+    function HomeController ($scope, Principal, LoginService, $state, Politic, ParseLinks, imageService, angularGridInstance) {
         var vm = this;
 
         vm.account = null;
@@ -42,6 +45,8 @@
         vm.reset = reset;
         vm.reverse = true;
 
+        vm.images = [];
+
         loadAll();
 
         function loadAll () {
@@ -63,6 +68,11 @@
                 vm.totalItems = headers('X-Total-Count');
                 for (var i = 0; i < data.length; i++) {
                     vm.politics.push(data[i]);
+                    data[i].actualWidth = 100;
+                    data[i].actualHeight = 100;
+                    if(data[i].photo == "")
+			data[i].photo = "http://sensoincomum.org/wp-content/uploads/2016/03/marketing-politico.jpg";
+		    vm.images.push(data[i]);
                 }
             }
 
@@ -74,6 +84,7 @@
         function reset () {
             vm.page = 0;
             vm.politics = [];
+            vm.images = [];
             loadAll();
         }
 
@@ -81,6 +92,28 @@
             vm.page = page;
             loadAll();
         }
+
+	imageService.loadImages().then(function(data){
+            data.data.items.forEach(function(obj){
+                var desc = obj.description,
+                    width = desc.match(/width="(.*?)"/)[1],
+                    height = desc.match(/height="(.*?)"/)[1];
+                
+                obj.actualHeight = height;
+                obj.actualWidth = width;
+            });
+           $scope.images = vm.images;// data.data.items;
+        });
+
+//intialized imageList
+$scope.page = 1;
+//method to load next data
+$scope.loadMore = function(){
+    loadPage(page).then(function(nextPageImages){
+        $scope.images = $scope.images.concat(nextPageImages);
+        $scope.page++;
+    });
+};
 
     }
 })();
